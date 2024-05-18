a3d24f5655c91c539d289157e87c5e500b2f5424
Robert <junhojeon010@gmail.com>
2024-05-17 20:57:49 -0600

    Modified function to create 16 x 16 grid of squares and added flex display to the grid

--- script.js
+++ script.js
@@ -4,13 +4,22 @@
 const container = document.createElement("div");
 document.body.appendChild(container)
 
-// ----- Creates 16 divs ----- //
+// ----- Creates 16 x 16 divs ----- //
 
-function divMaker(x) {
+function divMaker(x, y) {
     let i;
+    let j;
     for (i=0; i < x; i++) {
-        const divNum = document.createElement("div");
-        container.appendChild(divNum);
-    } 
-} divMaker(16);
+        const subtainer = document.createElement("div")
+        for (j=0; j < y; j++) {
+            const divNum = document.createElement("div");
+            divNum.classList.add("smallSquare")
+            divNum.style.height = "40px";
+            divNum.style.width = "40px";
+            divNum.style.border = "1px solid black";
+            subtainer.appendChild(divNum);
+    }   container.appendChild(subtainer);
 
+}} divMaker(16, 16);
+
+container.style.display = "flex";
